2a70847cdb5aeb9ff6067fb1835021681b5ca302
tradye <tradye@163.com>
2018-06-06 09:44:38 +0800

    error plot tool

---
+++ scripts_tools/plot_error.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+
+import math
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+# import rospy
+import csv
+import pandas as pd
+
+# APOLLO_ROOT = os.path.join(os.path.dirname(__file__), '../../../')
+
+# csv_file = csv.reader()
+lateral_error = pd.read_csv(filepath_or_buffer='/home/cidi-xh/xuhao/tmpfile/lqr_steer_angle.csv', sep = ',')["lateral_error"].values
+header_time = pd.read_csv(filepath_or_buffer='/home/cidi-xh/xuhao/tmpfile/lqr_steer_angle.csv', sep = ',')["control_time"].values
+heading_error = pd.read_csv(filepath_or_buffer='/home/cidi-xh/xuhao/tmpfile/lqr_steer_angle.csv', sep = ',')["heading_error"].values
+steering_angle = pd.read_csv(filepath_or_buffer='/home/cidi-xh/xuhao/tmpfile/lqr_steer_angle.csv', sep = ',')["steering"].values
+print(lateral_error)
+print(header_time)
+plt.figure()
+
+plt.subplot(2,1,1)
+plt.title('LQR Controller lateral errors')
+plt.xlabel('header time(s)')
+plt.ylabel('lateral error(m)/steer_angle')
+
+plt.plot(header_time, lateral_error, 'r', header_time, steering_angle, 'g--')
+plt.subplot(2, 1, 2)
+plt.title('LQR Controller heading errors')
+plt.xlabel('header time(s)')
+plt.ylabel('heading error(m)')
+plt.plot(header_time, heading_error)
+
+# plt.subplot(3,1,3)
+# plt.title('LQR Controller steering angle')
+# plt.xlabel('header time(s)')
+# plt.ylabel('steering angle(m)')
+# plt.plot(header_time, steering_angle)
+
+plt.legend()
+plt.show()
+
+# plt.plot(x, y, 'ro', x, y1, 'g--')
